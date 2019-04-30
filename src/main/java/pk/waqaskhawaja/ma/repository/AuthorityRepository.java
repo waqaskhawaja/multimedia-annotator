31782b5f89f70e80f338b93c30258724041432ac
Waqas Khawaja <waqas.kh@gmail.com>
2019-04-30 14:41:22 +0100

    Initial application generated by JHipster-5.8.2

---
+++ src/main/java/pk/waqaskhawaja/ma/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package pk.waqaskhawaja.ma.repository;
+
+import pk.waqaskhawaja.ma.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/pk/waqaskhawaja/ma/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package pk.waqaskhawaja.ma.security;
