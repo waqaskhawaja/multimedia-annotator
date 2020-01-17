280c73b7cbd983cf4bb7bc5e31ee4e52e8ce3cce
Waqas Khawaja <waqas.kh@gmail.com>
2020-01-17 14:33:46 +0000

    first includsion of yt-player-angular

--- src/main/webapp/app/entities/annotation-session/annotation-session-detail.component.ts
+++ src/main/webapp/app/entities/annotation-session/annotation-session-detail.component.ts
@@ -5,6 +5,7 @@ import { IAnnotationSession } from 'app/shared/model/annotation-session.model';
 import { AnnotationSessionService } from './annotation-session.service';
 import { IAnalysisSessionResource } from 'app/shared/model/analysis-session-resource.model';
 import { Options, LabelType, ChangeContext } from 'ng5-slider';
+import { YtPlayerService, PlayerOptions } from 'yt-player-angular';
 
 @Component({
     selector: 'jhi-annotation-session-detail',
@@ -30,7 +31,8 @@ export class AnnotationSessionDetailComponent implements OnInit {
     constructor(
         protected activatedRoute: ActivatedRoute,
         protected annotationSessionService: AnnotationSessionService,
-        protected embedService: EmbedVideoService
+        protected embedService: EmbedVideoService,
+        private ytPlayerService: YtPlayerService
     ) {}
 
     ngOnInit() {
@@ -40,6 +42,7 @@ export class AnnotationSessionDetailComponent implements OnInit {
         this.annotationSessionService.findVideoByAnalysisSession(this.annotationSession.analysisSession.id).subscribe(res => {
             this.analysisSessionResource = res.body;
             this.setSliderEnd(this.analysisSessionResource.url);
+            this.ytPlayerService.play();
         });
     }
 
